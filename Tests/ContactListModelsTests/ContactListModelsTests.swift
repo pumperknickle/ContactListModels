2869f8a3f0b3029582308a750ea0087c18e7c6a8
Pumper Knickle <pumperknickle@protonmail.com>
2020-03-04 20:13:31 -0800

    extensions

--- Tests/ContactListModelsTests/ContactListModelsTests.swift
+++ Tests/ContactListModelsTests/ContactListModelsTests.swift
@@ -6,7 +6,6 @@ final class ContactListModelsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ContactListModels().text, "Hello, World!")
     }
 
     static var allTests = [
